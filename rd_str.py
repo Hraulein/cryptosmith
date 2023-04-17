0536c1f7c4eee9610e6f87c36d26ba37c5fa8a1e
阿狸 <developer@hraulein.com>
2023-04-17 15:01:33 +0800

    something

--- rd_str.py
+++ rd_str.py
@@ -7,9 +7,11 @@ from lib import config_, enum_, utils_
 if __name__ == '__main__':
     yml = config_.YamlConfig('config/config.yml', 'RD_STR')
     config = yml.loaded_config()
-    # print(random_string(Level(config['level']), config['count'], config['bit']))
+    # 打印配置文件级别的密码
+    print(utils_.random_string(enum_.Level(config['level']), config['count'], config['bit']))
     # 打印全部级别的密码
-    for i in range(len(enum_.Level)):
-        print(f'【{enum_.Level(i + 1)}】')
-        print(utils_.random_string(enum_.Level(i + 1), config['count'], config['bit']))
+    # for i in range(len(enum_.Level)):
+    #     print(f'【{enum_.Level(i + 1)}】')
+    #     print(utils_.random_string(enum_.Level(i + 1), config['count'], config['bit']))
+
     utils_.console_pause()
